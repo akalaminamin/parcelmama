93347f4ce6246103af473619f4be90c35a970228
akalamin <akalaminamin91@gmail.com>
2022-06-26 14:56:32 -0700

    responsive navbar make

--- src/Shared/NavBar/NavBar.js
+++ src/Shared/NavBar/NavBar.js
@@ -1,107 +1,102 @@
-import React, {useEffect, useState} from "react";
-import { NavLink } from "react-router-dom";
-import { AiOutlineMenu } from "react-icons/ai";
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { FiMenu } from "react-icons/fi";
 import TopNavBar from "./TopNavBar";
 const NavBar = () => {
-  const [innerHeight, setInnerHeight] = useState(null);
-  const [sticky, setSticky] = useState(false);
-  useEffect(()=>{
-      if(window.scrollY > 200){
-        setInnerHeight(window.scrollY)
-        setSticky(true)
-      }else{
-        setSticky(false)
-      }
-  }, [innerHeight])
+  const [open, setOpen] = useState(false);
   return (
     <>
       <TopNavBar />
-      <header className={`bg-white py-2 shadow-md font-Rubik sticky top-0 left-0 w-full z-50 mb-16`}>
-        <nav className="container flex flex-col md:flex-row md:items-center items-start justify-between">
-          {/* <div className="flex items-center justify-between w-full">
-       <Link to="/">
-          <img
-            className="cursor-pointer w-2/3 md:w-full md:flex-auto flex-1"
-            src={require("../Assets/Images/logo.png")}
-            alt="main logo"
-          />
-        </Link>
-        {/* <span className="block md:hidden">
-        <AiOutlineMenu />
-        </span> */}
-          {/* </div>  */}
-          <NavLink to="/">
-            <img
-              className="cursor-pointer w-2/3 md:w-full md:flex-auto flex-1"
-              src={require("../../Assets/Images/logo.png")}
-              alt="main logo"
-            />
-          </NavLink>
-          <div className="menu">
-            <ul className="flex flex-col gap-5 md:flex-row md:gap-0">
-              <li className="list-none px-4">
-                <NavLink
+      <header
+        className={`shadow-md font-Rubik sticky top-0 left-0 w-full z-50`}
+      >
+        <nav className="bg-white shadow">
+          <div className="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
+            <div className="flex items-center justify-between">
+              <div>
+                <Link
+                  className="text-2xl font-bold text-gray-800 transition-colors duration-200 transform lg:text-3xl hover:text-gray-700"
                   to="/"
-                  className={({ isActive }) =>
-                    isActive ? "text-primary" : "hover:text-primary"
-                  }
+                >
+                  <img
+                    className="w-[80%]"
+                    src={require("../../Assets/Images/logo.png")}
+                    alt="logo"
+                  />
+                </Link>
+              </div>
+
+              {/* Mobile menu button */}
+              <div className="flex md:hidden">
+                <button
+                  type="button"
+                  className="text-gray-800 hover:text-gray-600 focus:outline-none focus:text-gray-600"
+                  aria-label="toggle menu"
+                  onClick={() => setOpen(!open)}
+                >
+                  <FiMenu />
+                </button>
+              </div>
+            </div>
+
+            {/* Mobile Menu open: "block", Menu closed: "hidden" */}
+            <div
+              className={`items-center md:flex mt-5 ${open ? "hidden" : ""}`}
+            >
+              <div className="flex flex-col md:flex-row md:mx-6 items-start md:items-center">
+                <NavLink
+                  className="my-1 text-sm font-medium text-gray-700 hover:text-primary transition-colors duration-200 transform  md:mx-4 md:my-0 border-b md:border-b-0 pb-4 md:pb-0 w-full md:w-auto"
+                  to="/"
+                  onClick={() => setOpen(!open)}
                 >
                   Home
                 </NavLink>
-              </li>
-              <li className="list-none px-4">
-                <a
-                  href="#ourServices"
-                  className={({ isActive }) =>
-                    isActive ? "text-primary" : "hover:text-primary"
-                  }
+                <NavLink
+                  className="my-1 text-sm font-medium text-gray-700 hover:text-primary transition-colors duration-200 transform  md:mx-4 md:my-0 border-b md:border-b-0 pb-4 md:pb-0 w-full md:w-auto"
+                  to="/services"
+                  onClick={() => setOpen(!open)}
                 >
                   Services
-                </a>
-              </li>
-              <li className="list-none px-4">
+                </NavLink>
                 <NavLink
+                  className="my-1 text-sm font-medium text-gray-700 hover:text-primary transition-colors duration-200 transform  md:mx-4 md:my-0 border-b md:border-b-0 pb-4 md:pb-0 w-full md:w-auto"
                   to="/servicesArea"
-                  className={({ isActive }) =>
-                    isActive ? "text-primary" : "hover:text-primary"
-                  }
+                  onClick={() => setOpen(!open)}
                 >
                   Services Area
                 </NavLink>
-              </li>
-              <li className="list-none px-4">
-                <a
-                  href="#pricing"
-                  className={({ isActive }) =>
-                    isActive ? "text-primary" : "hover:text-primary"
-                  }
+                <NavLink
+                  className="my-1 text-sm font-medium text-gray-700 hover:text-primary transition-colors duration-200 transform  md:mx-4 md:my-0 border-b md:border-b-0 pb-4 md:pb-0 w-full md:w-auto"
+                  to="/pricing"
+                  onClick={() => setOpen(!open)}
                 >
                   Pricing
-                </a>
-              </li>
-              <li className="list-none px-4">
+                </NavLink>
                 <NavLink
+                  className="my-1 text-sm font-medium text-gray-700 hover:text-primary transition-colors duration-200 transform  md:mx-4 md:my-0 border-b md:border-b-0 pb-4 md:pb-0 w-full md:w-auto"
                   to="/contact"
-                  className={({ isActive }) =>
-                    isActive ? "text-primary" : "hover:text-primary"
-                  }
+                  onClick={() => setOpen(!open)}
                 >
                   Contact Us
                 </NavLink>
-              </li>
-              <li className="list-none px-4">
-                <NavLink to="/register" className="nav-btn group">
+                <NavLink
+                  to="/register"
+                  onClick={() => setOpen(!open)}
+                  className="nav-btn group mr-0 md:mr-5 mt-5 md:mt-0 w-full md:w-auto text-center"
+                >
                   <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-20 ease"></span>
                   <span className="relative">Register</span>
                 </NavLink>
-              </li>
-              <li className="list-none px-4">
-                <NavLink to="/login" className="nav-btn group">
+                <NavLink
+                  to="/login"
+                  onClick={() => setOpen(!open)}
+                  className="nav-btn group mt-5 md:mt-0 w-full md:w-auto text-center"
+                >
                   <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-20 ease"></span>
                   <span className="relative">Login</span>
                 </NavLink>
-              </li>
-            </ul>
+              </div>
+            </div>
           </div>
         </nav>
       </header>
